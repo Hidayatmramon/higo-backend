36ff4c2aaf50935b77a1f3a3cc6a6e2ceab46df4
Ramon hidayat <74350696+Hidayatmramon@users.noreply.github.com>
2025-07-13 12:33:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected!');
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error('MongoDB connection error:', err));
